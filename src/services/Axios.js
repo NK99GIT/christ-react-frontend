5b92a2dcd53aa87475cbeb9b4da6acdc3b975f00
Kathir1999 <113663134+Kathir1999@users.noreply.github.com>
2025-07-19 19:50:37 +0530

    Your change description

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://3.107.89.15:3306/api/",
+  baseURL: "http://3.107.89.15:5001/api/",
   headers: {
     "Content-Type": "application/json",
   },
